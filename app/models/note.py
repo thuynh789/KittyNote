84be0bb437083922b359228689695a83576cb9b7
tiana3113 <t.huynh3113@gmail.com>
2023-02-21 13:24:26 -0800

    note db model

---
+++ app/models/note.py
@@ -0,0 +1,42 @@
+from .db import db, environment, SCHEMA, add_prefix_for_prod
+from datetime import datetime
+
+class Note(db.Model):
+    __tablename__ = "notes"
+
+    if environment == "production":
+        __table_args__ = {'schema': SCHEMA}
+
+    id = db.Column(db.Integer, primary_key=True)
+    userId = db.Column(db.Integer, db.ForeignKey(add_prefix_for_prod('users.id')), nullable=False)
+    notebookId = db.Column(db.Integer, db.ForeignKey(add_prefix_for_prod('notebooks.id')), nullable=False)
+    title = db.Column(db.String(100), nullable=False)
+    content = db.Column(db.String(1000), nullable=False)
+    created_at = db.Column(db.DateTime, default=datetime.now())
+    updated_at = db.Column(db.DateTime, default=datetime.now(), onupdate=datetime.now())
+
+
+    user = db.relationship("User", back_populates="note")
+    notebook = db.relationship("Notebook", back_populates="note")
+
+    def format_date(self, date):
+        date_year = date.year
+        today = datetime.now()
+        today_year = today.strftime("%Y")
+        if str(date_year) == str(today_year):
+            return date.strftime("%b %d")
+        else:
+            return date.strftime("%b %d, %Y")
+
+
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'userId': self.userId,
+            'notebookId': self.notebookId,
+            'title': self.title,
+            'content': self.content,
+            'user_id': self.user_id,
+            'created_at': self.format_date(self.created_at),
+            'updated_at': self.format_date(self.updated_at)
+        }
