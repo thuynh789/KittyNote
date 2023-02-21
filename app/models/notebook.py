1c4bf20c194593c9b76d143e07e69758c158c42c
tiana3113 <t.huynh3113@gmail.com>
2023-02-21 14:15:42 -0800

    notebook model

--- app/models/notebook.py
+++ app/models/notebook.py
@@ -1,23 +1,21 @@
 from .db import db, environment, SCHEMA, add_prefix_for_prod
 from datetime import datetime
 
-class Note(db.Model):
-    __tablename__ = "notes"
+class Notebook(db.Model):
+    __tablename__ = "notebooks"
 
     if environment == "production":
         __table_args__ = {'schema': SCHEMA}
 
     id = db.Column(db.Integer, primary_key=True)
     userId = db.Column(db.Integer, db.ForeignKey(add_prefix_for_prod('users.id')), nullable=False)
-    notebookId = db.Column(db.Integer, db.ForeignKey(add_prefix_for_prod('notebooks.id')), nullable=False)
     title = db.Column(db.String(100), nullable=False)
-    content = db.Column(db.String(1000), nullable=False)
     created_at = db.Column(db.DateTime, default=datetime.now())
     updated_at = db.Column(db.DateTime, default=datetime.now(), onupdate=datetime.now())
 
+    user = db.relationship("User", back_populates="notebook")
+    note = db.relationship("Note", back_populates="notebook", cascade="all, delete")
 
-    user = db.relationship("User", back_populates="note")
-    notebook = db.relationship("Notebook", back_populates="note")
 
     def format_date(self, date):
         date_year = date.year
@@ -33,9 +31,8 @@ class Note(db.Model):
         return {
             'id': self.id,
             'userId': self.userId,
-            'notebookId': self.notebookId,
             'title': self.title,
-            'content': self.content,
             'created_at': self.format_date(self.created_at),
-            'updated_at': self.format_date(self.updated_at)
+            'updated_at': self.format_date(self.updated_at),
+            "notes": [note.to_dict() for note in self.notes]
         }
