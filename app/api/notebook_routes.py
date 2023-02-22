bf5b74ea099a00d657ed9987242c06772a4a0e70
tiana3113 <t.huynh3113@gmail.com>
2023-02-21 20:10:27 -0800

    get one notebook

--- app/api/notebook_routes.py
+++ app/api/notebook_routes.py
@@ -22,4 +22,25 @@ def validation_errors_to_error_messages(validation_errors):
 @login_required
 def get_user_notebooks():
     allNotebooks = Notebook.query.filter(Notebook.userId == current_user.id).all()
-    return {"Notebooks": [notebook.to_dict() for notebook in allNotebooks]}
+    return jsonify({"Notebooks": [notebook.to_dict() for notebook in allNotebooks]})
+
+
+# GET ONE NOTEBOOK
+@notebook_routes.route("/<int:id>")
+@login_required
+def get_one_notebook(id):
+    oneNotebook = Notebook.query.get(id)
+    if oneNotebook is None:
+        return jsonify({'error': 'Notebook not found'}),404
+    if oneNotebook.userId != current_user.id:
+        return jsonify({'error': 'Unauthorized'}), 401
+    notes = [note.to_dict() for note in oneNotebook.note]
+    # notes = [note.to_dict() for note in oneNotebook.note]
+    return jsonify({
+        'Notebook': {
+            'id': oneNotebook.id,
+            'title': oneNotebook.title,
+            'created_at': oneNotebook.created_at,
+            'updated_at': oneNotebook.updated_at,
+            'notes': notes
+        }})
