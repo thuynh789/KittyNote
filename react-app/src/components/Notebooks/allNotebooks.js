61c105e010bd5fa6c5558b0cb1fd06588ed3da91
tiana3113 <t.huynh3113@gmail.com>
2023-02-22 15:15:23 -0800

    background image

--- react-app/src/components/Notebooks/allNotebooks.js
+++ react-app/src/components/Notebooks/allNotebooks.js
@@ -1,14 +1,21 @@
-// import { useState, useEffect } from "react";
-// import { useSelector, useDispatch } from "react-redux";
-// import { getUserNotebooks_thunk } from "../../store/note";
-// import { useHistory } from "react-router-dom";
+import { useState, useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { getUserNotebooks_thunk } from "../../store/note";
+import { useHistory } from "react-router-dom";
 
-// export default function allNotebooks() {
-//     const dispatch = useDispatch();
-//     const history = useHistory();
-//     const user = useSelector((state) => state.session.user);
+export default function allNotebooks() {
+    const dispatch = useDispatch();
+    const history = useHistory();
+    const user = useSelector((state) => state.session.user);
+    const notebooks = useSelector((state) =>state.notebooks.allNotebooks);
 
-//     useEffect(() => {
-//         dispatch(getUserNotebooks_thunk());
-//       }, [dispatch]);
-// }
+    useEffect(() => {
+        dispatch(getUserNotebooks_thunk());
+      }, [dispatch]);
+
+    return (
+        <div className = "notebook-container">
+            {notebooks}
+        </div>
+    )
+}
