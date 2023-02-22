306bca45b3994966fa1e990fd15791dd20f71134
tiana3113 <t.huynh3113@gmail.com>
2023-02-21 19:08:52 -0800

    get all notebooks for user route

--- app/api/notebook_routes.py
+++ app/api/notebook_routes.py
@@ -3,4 +3,23 @@ from flask_login import login_required, current_user
 from app.models import Notebook, Note, User, db
 from app.forms import NotebookForm
 
-notebooks_routes = Blueprint('notebooks', __name__)
+
+notebook_routes = Blueprint('notebooks', __name__)
+
+def validation_errors_to_error_messages(validation_errors):
+    """
+    Simple function that turns the WTForms validation errors into a simple list
+    """
+    errorMessages = []
+    for field in validation_errors:
+        for error in validation_errors[field]:
+            errorMessages.append(f'{field} : {error}')
+    return errorMessages
+
+
+# GET NOTEBOOKS OF CURRENT USER
+@notebook_routes.route("/")
+@login_required
+def get_user_notebooks():
+    allNotebooks = Notebook.query.filter(Notebook.userId == current_user.id).all()
+    return {"Notebooks": [notebook.to_dict() for notebook in allNotebooks]}
