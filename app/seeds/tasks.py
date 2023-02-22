f939bcb5c20e46da2b94c3e006b2495681d0ce1a
tiana3113 <t.huynh3113@gmail.com>
2023-02-21 16:30:46 -0800

    task seeds

--- app/seeds/tasks.py
+++ app/seeds/tasks.py
@@ -4,12 +4,24 @@ def seed_tasks():
 
     task_1 = Task(
         userId= 1,
-        title= 'Purrfect Pages',
-        is_completed=None
+        title= 'Meow for food.',
+        is_completed=True
+    )
+    task_2 = Task(
+        userId= 1,
+        title= 'Lay on keyboard',
+        is_completed=False
+    )
+    task_3 = Task(
+        userId= 1,
+        title= 'Catch mice and bring to humans',
+        is_completed=False
     )
 
 
     db.session.add(task_1)
+    db.session.add(task_2)
+    db.session.add(task_3)
     db.session.commit()
 
 
