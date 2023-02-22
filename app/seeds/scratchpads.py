97b876f1618a25ba87041720e6cf097a5af0aca4
tiana3113 <t.huynh3113@gmail.com>
2023-02-21 16:35:21 -0800

    scratchpad seeds

---
+++ app/seeds/scratchpads.py
@@ -0,0 +1,30 @@
+from app.models import db, Scratchpad, environment, SCHEMA
+
+def seed_scratchpads():
+
+    task_1 = Scratchpad(
+        userId= 1,
+        content= ''
+    )
+    task_2 = Scratchpad(
+        userId= 2,
+        content= ''
+    )
+    task_3 = Scratchpad(
+        userId= 3,
+        content= ''
+    )
+
+    db.session.add(scratchpad_1)
+    db.session.add(scratchpad_2)
+    db.session.add(scratchpad_3)
+    db.session.commit()
+
+
+def undo_scratchpads():
+    if environment == "production":
+        db.session.execute(f"TRUNCATE table {SCHEMA}.scratchpads RESTART IDENTITY CASCADE;")
+    else:
+        db.session.execute("DELETE FROM scratchpads")
+
+    db.session.commit()
