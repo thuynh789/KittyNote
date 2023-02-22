4b654f9c34abafdad92274ec9a2dc12b0989ec41
tiana3113 <t.huynh3113@gmail.com>
2023-02-21 16:37:38 -0800

    scratchpad update models

--- app/models/scratchpad.py
+++ app/models/scratchpad.py
@@ -9,7 +9,7 @@ class Scratchpad(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
     userId = db.Column(db.Integer, db.ForeignKey(add_prefix_for_prod('users.id')), nullable=False)
-    content = db.Column(db.String(1000), nullable=False)
+    content = db.Column(db.Text)
     created_at = db.Column(db.DateTime, default=datetime.now())
     updated_at = db.Column(db.DateTime, default=datetime.now(), onupdate=datetime.now())
 
