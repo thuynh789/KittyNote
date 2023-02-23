55e7803067075ebe262d3f1cbb1ba782e0de9d07
tiana3113 <t.huynh3113@gmail.com>
2023-02-22 21:35:43 -0800

    images added for background

--- react-app/src/components/LandingPage/LandingPage.js
+++ react-app/src/components/LandingPage/LandingPage.js
@@ -7,56 +7,65 @@ import LoginFormModal from "./LoginFormModal/index";
 import SignupFormModal from "./SignupFormModal/index";
 import DemoUser from "../auth/DemoUser";
 import "./LandingPage.css"
+import logo from "../Images/logo.png"
+import outline from "../Images/outline.png"
 
 export default function LandingPage() {
   return (
     <div className="splash-container">
-      <div className="splash-nav">
-        <div className="nav-header">
-          <div className="splash-name">
-            <h2>KittyNote</h2>
-          </div>
-        </div>
-        <div className="splash-login">
-          <OpenModalButton
-            buttonText="Login"
-            className="sign-in register"
-            modalComponent={<LoginFormModal />}
-          />
-        </div>
-      </div>
-      <div className="splash-signup">
-        <OpenModalButton
-          buttonText="Signup"
-          className="signup"
-          modalComponent={<SignupFormModal />}
-        />
-      </div>
-        <div className="splash" >
-            <DemoUser />
+
+        <div className="splash-logo">
+           <img src={logo} alt='logo' />
         </div>
 
-      <footer className="splash-footer">
-        <div className="leftside-footer">Evernote clone by Tiana Huynh</div>
-        <div className="rightside-footer">
-          <a
-            className="link-footer"
-            href="https://www.linkedin.com/in/tiana-huynh-58b296168/"
-            rel="noreferrer"
-            target="_blank"
-          >
-            <i className="fa-brands fa-linkedin fa-xl" />
-          </a>
-          <a
-            className="link-footer"
-            href="https://github.com/thuynh789/KittyNote"
-            rel="noreferrer"
-            target="_blank"
-          >
-            <i className="fa-brands fa-github fa-xl" />
-          </a>
+        <div className="splash">
+            <img src={outline} alt='outline' />
+            <div className="buttons">
+
+                <div className="button1">
+                <OpenModalButton
+                    buttonText="Login"
+                    className="sign-in register"
+                    modalComponent={<LoginFormModal />}
+                />
+                </div>
+
+                <div className="button2">
+                <OpenModalButton
+                 buttonText="Signup"
+                 className="signup"
+                modalComponent={<SignupFormModal />}
+                />
+                </div>
+                <div className="button3">
+                    <DemoUser />
+                </div>
+
+                <div className="footers">
+                    <div className="footer">Evernote clone by Tiana Huynh</div>
+                    <div className="rightside-footer">
+                    <a
+                        className="link-footer"
+                        href="https://www.linkedin.com/in/tiana-huynh-58b296168/"
+                        rel="noreferrer"
+                        target="_blank"
+                    >
+                        <i className="fa-brands fa-linkedin fa-xl" />
+                    </a>
+                    <a
+                        className="link-footer"
+                        href="https://github.com/thuynh789/KittyNote"
+                        rel="noreferrer"
+                        target="_blank"
+                    >
+                        <i className="fa-brands fa-github fa-xl" />
+                    </a>
+                    </div>
+                </div>
+
+            </div>
         </div>
-      </footer>
+
     </div>
   );
 }
