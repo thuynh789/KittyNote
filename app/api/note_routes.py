7333a5d87704ffc5d76df5c2b047071a27a2a39d
tiana3113 <t.huynh3113@gmail.com>
2023-02-25 14:19:15 -0800

    get notes routes done

---
+++ app/api/note_routes.py
@@ -0,0 +1,85 @@
+from flask import Blueprint, jsonify, request, session, redirect
+from flask_login import login_required, current_user
+from app.models import Notebook, Note, User, db
+from app.forms import NoteForm
+from app.api.auth_routes import validation_errors_to_error_messages
+
+
+note_routes = Blueprint('notes', __name__)
+
+
+# GET NOTES OF CURRENT USER
+@note_routes.route("/")
+@login_required
+def get_user_notes():
+    print('WE IN THE NOTES ROUTE')
+    allNotes = Note.query.filter(Note.userId == current_user.id).all()
+    print(allNotes)
+    return jsonify({"Notes": [note.to_dict() for note in allNotes]})
+
+
+# GET ONE NOTEBOOK
+@note_routes.route("/<int:id>")
+@login_required
+def get_one_note(id):
+    print('--------WE ARE IN NOTEBOOK DETAILS ROUTE---------')
+    oneNote = Note.query.get(id)
+    if oneNote is None:
+        return jsonify({'error': 'Note not found'}),404
+    if oneNote.userId != current_user.id:
+        return jsonify({'error': 'Unauthorized'}), 401
+    note = oneNote.to_dict()
+    return jsonify({ 'Note': note })
+
+
+# #CREATE NOTEBOOK
+# @notebook_routes.route("/", methods=['POST'])
+# @login_required
+# def create_notebook():
+#     form = NotebookForm()
+#     form['csrf_token'].data = request.cookies['csrf_token']
+#     data = form.data
+
+#     if form.validate_on_submit():
+#         notebook = Notebook(
+#             title=data['title'],
+#             userId=current_user.id,
+#         )
+#         db.session.add(notebook)
+#         db.session.commit()
+#         return notebook.to_dict()
+#     return {'errors': validation_errors_to_error_messages(form.errors)}, 400
+
+
+
+# #UPDATE/EDIT NOTEBOOK
+# @notebook_routes.route("/<int:id>", methods=['PUT'])
+# @login_required
+# def update_notebook(id):
+#     notebook = Notebook.query.get(id)
+#     if notebook is None:
+#         return jsonify({'error': 'Notebook not found'}),404
+#     if current_user.id != notebook.userId:
+#         return jsonify({'error': 'Unauthorized'}), 401
+
+#     form = NotebookForm()
+#     form['csrf_token'].data = request.cookies['csrf_token']
+#     if form.validate_on_submit():
+#         notebook.title = form.data['title']
+#         db.session.commit()
+#         return notebook.to_dict()
+#     return {'errors': validation_errors_to_error_messages(form.errors)}, 400
+
+
+# #DELETE NOTEBOOK
+# @notebook_routes.route("/<int:id>", methods=['DELETE'])
+# @login_required
+# def delete_notebook(id):
+#     notebook = Notebook.query.get(id)
+#     if notebook is None:
+#         return jsonify({'error': 'Notebook not found'}),404
+#     if current_user.id != notebook.userId:
+#         return jsonify({'error': 'Unauthorized'}), 401
+#     db.session.delete(notebook)
+#     db.session.commit()
+#     return jsonify({"message": "Notebook deleted"}), 200
--- react-app/src/components/Navigation/index.js
+++ react-app/src/components/Navigation/index.js
@@ -16,23 +16,23 @@ function Navigation(){
 					<img src={logo} alt='logo' />
 				</NavLink>
 			</div>
-			<div>
+			<div className='nav1'>
 				Hi {sessionUser?.username} !
 			</div>
-			<div>
+			<div className='nav2'>
 				<NavLink exact to="/home">Home</NavLink>
 			</div>
-			<div>
+			<div className='nav3'>
           		<NavLink className="navlink" to="/notebooks">
               	Notebooks
           		</NavLink>
        		</div>
-			<div>
+			<div className='nav4'>
           		<NavLink className="navlink" to="/notes">
               	Notes
           		</NavLink>
        		</div>
-			<div>
+			<div className='nav'>
 				<LogoutButton/>
 			</div>
 		</ul>
