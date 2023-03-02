0c4f071edaecbde29ad7f64e884f71a6daa7b9d7
tiana3113 <t.huynh3113@gmail.com>
2023-03-01 22:04:39 -0800

    no notes message for notes page

--- react-app/src/components/Notebooks/NotebookDetails.js
+++ react-app/src/components/Notebooks/NotebookDetails.js
@@ -2,13 +2,14 @@ import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { getOneNotebook_thunk } from "../../store/notebooks";
 import { useParams } from "react-router-dom";
+import { Link } from "react-router-dom";
 import "./Notebooks.css"
 
 export default function NotebookDetails(){
     const myNotebook = useSelector(state => state.notebooks.singleNotebook)
     const myNotes = useSelector(state => state.notebooks.singleNotebook.notes)
     // console.log(myNotebook)
-    // console.log(myNotes)
+    console.log(myNotes)
     const dispatch = useDispatch();
     const {notebookId} = useParams();
 
@@ -27,12 +28,15 @@ export default function NotebookDetails(){
         <div className='notebook-container'>
             <div className='single-notebook-header'>
                 <h1>{myNotebook.title}</h1>
+                <h2>{myNotes?.length} Notes</h2>
             </div>
             <div classNAme='note-page'>
-                {myNotebook.created_at}
-                <ul>
+                Created at: {' '}{myNotebook.created_at}
+                <ul className="nbnotes" style={{marginTop: '2rem'}}>
                 {myNotes?.map(note => (
-                    <li key={note.content}>{note.title}: {note.content}</li>
+                    <li className= 'nbnotes2' style={{marginTop: '1rem'}} key={note.id}>
+                       <Link to={`/notebooks/${myNotebook.id}/${note.id}`}>{note.title}</Link>
+                    </li>
                 ))}
                 </ul>
             </div>
