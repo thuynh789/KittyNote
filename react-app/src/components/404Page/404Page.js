188d90f0fd3e8386ab8eea0ec5b82fe96f2af14f
tiana3113 <t.huynh3113@gmail.com>
2023-04-01 16:07:27 -0400

    404 page done

--- react-app/src/components/404Page/404Page.js
+++ react-app/src/components/404Page/404Page.js
@@ -1,21 +1,27 @@
 import cat from '../Images/oh-no.png'
 import { Link } from 'react-router-dom';
+import './404Page.css'
 
 export default function ErrorCat() {
 	return (
-		<div id='fullErrorDiv'>
+		<div className='fullErrorDiv'>
 			<div className='errorImgDiv'>
-			<img
-				src= {cat}
-				alt='lost cat'
-				/>
-            <div>
-            Meow-sorry! Can't find what you're looking for, hooman.
-            <br />
-            Maybe my fluffy paws accidentally pressed a wrong button.
+                <img
+                    src= {cat}
+                    alt='lost cat'
+                    />
+                <div className='message'>
+                    <div className='four'>
+                        404 Page Not Found
+                    </div>
+                    Meow-sorry! Can't find what you're looking for, hooman.
+                    <br />
+                    Maybe my fluffy paws accidentally pressed a wrong button. 😿
+                    <div className='redirect'>
+                        <Link to='/home'>Click here to return to Home</Link>
+                    </div>
+			    </div>
             </div>
-			</div>
-			<Link to='/home'>Click here to return to Home</Link>
 		</div>
 	);
 }
